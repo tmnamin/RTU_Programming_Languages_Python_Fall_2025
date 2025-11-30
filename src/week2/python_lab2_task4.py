f5a759d3308b94d52e6d23e157e43b11219d8911
Amin Naghizade <nagizade.amin@gmail.com>
2025-11-30 01:17:32 +0000

    Solved lab2_task4

--- src/week2/python_lab2_task4.py
+++ src/week2/python_lab2_task4.py
@@ -21,17 +21,17 @@ prices = [12.5, 9.9, 15.0, 22.3, 5.0]
 quantities = [2, 5, 1, 3, 4]
 
 # TODO: Compute totals using map()
-totals = []
+totals = [price * quantity for price, quantity in zip(prices, quantities)]
 
 # TODO: Filter totals above 30
-high_totals = []
+high_totals = [total for total in totals if total > 30]
 
 # TODO: Pair prices and quantities with zip()
-pairs = []
+pairs = [ (price, quantity) for price, quantity in zip(prices, quantities)]
 
 # TODO: Repeat using list comprehensions
-totals_comp = []
-high_totals_comp = []
+totals_comp = [price * quantity for price, quantity in zip(prices, quantities)]
+high_totals_comp = [total for total in totals_comp if total > 30]
 
 # TODO: Print results
 print("Totals:", totals)
